76a765d7d3278db9481268332e57eb0b6a1d728f
omarfathy56 <eng.omarfathy98697#gmail.com>
2024-12-19 11:54:15 +0200

    version1

---
+++ main.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import streamlit as st
+
+
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv("clients_data.csv")
+    df.columns = ["Name", "BPM", "SpO2"]
+    return df
+data = load_data()
+
+st.sidebar.subheader("📞 Call Us ")
+st.sidebar.markdown("01050605580")
+st.sidebar.markdown("📧 eng.omarfathy98697@gmail.com")
+
+
+with st.container():
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image("LHO.webp", width=500)
+    with col2:
+        st.markdown("<h1 style'text-align: right; color: red;'>Welcome to Local Health Organization</h1>",
+                    unsafe_allow_html=True)
+
+st.subheader("Welcome to LHO : ")
+st.markdown("we here help people to measure their heart rate state ,O2 and temperature in their body , \n we hope we "
+            "will be useful to all people and help them as far as possible ")
+
+
+st.write("Clients Data:")
+st.dataframe(data, width=1000)
+
+
---
+++ pages/clients.py
@@ -0,0 +1,33 @@
+import main
+import streamlit as st
+import plotly.express as px
+
+data = main.load_data()
+
+st.sidebar.subheader("📞 Call Us ")
+st.sidebar.markdown("01050605580")
+st.sidebar.markdown("📧 eng.omarfathy98697@gmail.com")
+
+
+list_of_names = [name for name in data["Name"].unique()]
+
+def make_clinet(num_of_clinet, client):
+    with st.container():
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image("photo.jpg", width=100)
+        with col2:
+            st.markdown(f"<h1 style'text-align: right; color: red;'>{num_of_clinet}.{client}</h1>",
+                        unsafe_allow_html=True)
+
+    st.dataframe(data[data["Name"] == client], width=1000)
+    fig = px.scatter(data[data["Name"] == client], x='BPM', y='SpO2', title='BPM vs SpO2')
+
+    st.plotly_chart(fig)
+    st.image("gif.gif")
+
+for num in range(len(list_of_names)):
+    make_clinet(num+1, list_of_names[num])
+
+
+
